85866818141574ed036f403f1192226701efd4a3
Kirill Shiryaev <shiryaev.kirill.a@gmail.com>
2024-03-26 20:48:44 +0100

    first commit

---
+++ Features/check.py
@@ -0,0 +1,10 @@
+def check_num(ans, lst):
+    while ans not in lst:
+        ans = input("Число введено неверно: ")
+    return ans
+
+def func():
+    pass
+
+# if __name__ == "__main__":
+#     check_num(0, ["1", "2"])
---
+++ Features/dict_features.py
@@ -0,0 +1,10 @@
+def add_words_json():
+    pass
+
+def change_words_json():
+    pass
+
+
+
+if __name__ == "__main__":
+    print(1)
